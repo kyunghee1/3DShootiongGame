5a4873d5f297a469a9a35b00219fa26d7c406efb
DESKTOP-358CSLP\USER_kyunghee1 <sancenim2019@gamil.com>
2024-02-20 11:04:09 +0900

    드럼

--- Assets/Scenes/02.Scripts/Drum.cs
+++ Assets/Scenes/02.Scripts/Drum.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Threading;
+
+
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -15,44 +16,76 @@ public class Drum : MonoBehaviour, IHitable
     public int Damage = 70;
     public float ExplosionRadius = 10f;
 
-   public void Start()
+    private bool _isExplosion = false;
+
+    public void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
+      
     }
+    
+    
     public void Hit (int damage)
     {
         _hitCount += 1;
         if (_hitCount >=3)
         {
-          
-            Destroy(gameObject, damage);
-            Kill();
+
+            Explosion();
+
         }
     }
-    private void Kill()
+ 
+    private void Explosion()
     {
+        if (_isExplosion)
+        {
+            return;
+        }
+        _isExplosion = true;
+
         GameObject explosion = Instantiate(ExplosionParticlePrefabs);
 
         explosion.transform.position = this.transform.position;
         _rigidbody.AddForce(Vector3.up * UpPower, ForceMode.Impulse);
         _rigidbody.AddTorque(new Vector3(1, 0, 1) * UpPower / 2f);
-    
+
         //1.폭발 범위 내 콜라이더 찾기
-        int findLayer = LayerMask.GetMask("Player") | LayerMask.GetMask("monster");
+        int findLayer = LayerMask.GetMask("Player") | LayerMask.GetMask("monster") ;
+        
         Collider[] colliders = Physics.OverlapSphere(transform.position, ExplosionRadius, findLayer);
         //2. 콜라이더 내에서 Hitable 찾기
         foreach (Collider c in colliders)
         {
+            int i = 0;
             IHitable hitable = null;
-            if (c.TryGetComponent<IHitable>(out hitable))
+            if (c.TryGetComponent<IHitable>(out hitable) | i < colliders.Length)
             {
                 //3. 데미지 주기
                 hitable.Hit(Damage);
+            
             }
 
         }
-        Destroy(gameObject, 3f);
+        Destroy(gameObject);
+        int environmentLayer = LayerMask.GetMask("Environment");
+        Collider[] environmentColliders = Physics.OverlapSphere(transform.position, ExplosionRadius, environmentLayer);
+        foreach (Collider c in environmentColliders)
+        {
+            Drum drum = null;
+            if (c.TryGetComponent(out drum))
+            {
+                drum.Explosion();
+            }
+        }
+        StartCoroutine(Kill_Coroutine());
     }
-        
-   
+
+    private IEnumerator Kill_Coroutine()
+    {
+        yield return new WaitForSeconds(3f);
+        Destroy(gameObject);
+    }
 }
+   
+
--- Assets/Scenes/02.Scripts/WeaPon/Bomb.cs
+++ Assets/Scenes/02.Scripts/WeaPon/Bomb.cs
@@ -19,6 +19,7 @@ public class Bomb : MonoBehaviour
     //자기 자신의 게임 오브젝트를 사라지게 하는 코드
     public GameObject BombEffectPrefab;
     public int Damage = 60;
+    private Collider[] _collidedrs = new Collider[10];
     
 
 
@@ -35,15 +36,16 @@ public class Bomb : MonoBehaviour
 
         //2. 범위안에 있는 모든 콜라이더를 찾는다.
         int layer = LayerMask.GetMask("Monster") | LayerMask.NameToLayer("Player");
-        Collider[] colliders = Physics.OverlapSphere(transform.position, ExplsionRadius, layer); //| 비트합 연산자//LayerMask. 1 << 8, 1<<9
+        int count = Physics.OverlapSphereNonAlloc(transform.position, ExplsionRadius, _collidedrs ,layer); //| 비트합 연산자//LayerMask. 1 << 8, 1<<9
         //-> 피직스.오버랩 함수는 특정 영역(radius)안에 있는 특정 레이어들의 게임 오브젝트의
         //콜라이더 컴포넌트들을 모두 찾아 배열로 반환하는 함수
         //영역의 형태: 스피어, 큐브, 캡슐
 
         //3. 찾은 콜라이더 중에서 타격 가능한(IHitable) 오브젝트를 찾는다.
-        foreach(Collider collider in colliders)
+        for(int i = 0; i < count; i++)
             {
-            IHitable hitable = collider.GetComponent<IHitable>();
+            Collider c = _collidedrs[i];
+           IHitable hitable = c.GetComponent<IHitable>();
             if (hitable != null)
             {
                 hitable.Hit(Damage);
